d944df2347371ee05f035d1cb6a4f47a7ad5b817
Harry McNinson <50222922+hmcninson@users.noreply.github.com>
2025-08-30 21:36:50 -0400

    initial commit

---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.nnipa.proto.auth"},{"l":"com.nnipa.proto.command"},{"l":"com.nnipa.proto.common"},{"l":"com.nnipa.proto.tenant"}];updateSearchResults();
